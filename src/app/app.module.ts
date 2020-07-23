c079b6e0067c0de06fa0de9eb72995b558d284a9
Victor Garcia <vgarciainfante@gmail.com>
2020-07-22 22:43:30 -0400

    home page - first section ready. Responsive is pending!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { HomeComponent } from './components/home/home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
