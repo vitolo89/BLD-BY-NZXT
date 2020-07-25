d3ac62d3cfefe8cb4513b482ef2afe85b646216d
Victor Garcia <vgarciainfante@gmail.com>
2020-07-25 19:56:29 -0400

    section brands and check out ready, responsive done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer/footer.component';
 import { HomeComponent } from './components/home/home/home.component';
+import { BrandsComponent } from './components/brands/brands/brands.component';
 
 
 
@@ -14,7 +15,8 @@ import { HomeComponent } from './components/home/home/home.component';
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    BrandsComponent
   ],
   imports: [
     BrowserModule,
